3b0e8c1f26d2b90a6238c6c9f2ec67ecc9bcac26
Hamza Mateen <hamzamateen722@gmail.com>
2024-01-26 16:34:21 +0500

    app colors added

--- lib/helper/app_colors.dart
+++ lib/helper/app_colors.dart
@@ -1,3 +1,15 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
+  static final Color buttonColor = Colors.deepPurple;
+  static final Color textColor = Colors.black54;
+  static final Color buttonTextColor = Colors.white;
+  static final Color headingColor = Colors.black;
+  static final Color iconColor = Colors.deepPurple;
+  static final Color activeTextFieldColor = Colors.deepPurple;
+  static final Color nonActiveTextFieldColor = Colors.black54;
+  static final Color detailsIconColor = Colors.black;
+  static final Color loginTextColor = Colors.orange;
+  static final Color promoCodeColor = Colors.yellow;
 
 }
