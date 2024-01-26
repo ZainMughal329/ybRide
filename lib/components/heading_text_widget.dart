99bcc6bda33fe1685c494fc22ccf46e9788009ee
Hamza Mateen <hamzamateen722@gmail.com>
2024-01-26 18:29:33 +0500

    create reuseable widgets

---
+++ lib/components/heading_text_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HeadingTextWidget extends StatelessWidget {
+  final String title;
+  final Color textColor;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final TextDecoration decoration;
+  final Color decorationColor;
+  final FontStyle fontStyle;
+  final TextAlign? textAlign;
+
+  const HeadingTextWidget(
+      {super.key,
+        required this.title,
+        this.textColor = Colors.black,
+        this.fontSize = 18,
+        this.fontWeight = FontWeight.bold,
+        this.decoration = TextDecoration.none,
+        this.decorationColor = Colors.black,
+        this.fontStyle = FontStyle.normal,
+        this.textAlign,
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      style: GoogleFonts.montserrat(
+        decoration: decoration,
+        decorationColor: decorationColor,
+        color: textColor,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        fontStyle: fontStyle,
+
+      ),
+    );
+  }
+}
