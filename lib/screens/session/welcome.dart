24619dbaa9d4f31114cabce74fc7246af96b209f
Hamza Mateen <hamzamateen722@gmail.com>
2024-01-26 18:48:21 +0500

    welcome page

---
+++ lib/screens/session/welcome.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image.asset('assets/images'),
+          ),
+        ],
+      ),
+    );
+  }
+}
