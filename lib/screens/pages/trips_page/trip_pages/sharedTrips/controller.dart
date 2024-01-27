e5fadb0d0ecb017a5ceb1cb0e2d5a5692415dd5f
Hamza Mateen <hamzamateen722@gmail.com>
2024-01-27 14:37:38 +0500

    trips screens

--- lib/screens/pages/trips_page/trip_pages/sharedTrips/controller.dart
+++ lib/screens/pages/trips_page/trip_pages/sharedTrips/controller.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:yb_ride/screens/pages/trips_page/inded.dart';
 import 'package:yb_ride/screens/pages/trips_page/trip_pages/sharedTrips/inded.dart';
 
 class SharedTripController extends GetxController {
